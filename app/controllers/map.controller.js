340f9eeb8e30751f6396bef4ad5ab6d1371222e8
Jon Toy <jonathon.toy@gmail.com>
2018-05-04 19:09:46 -0700

    Sanitize feature function with decimal reduced to 6 digits

--- app/controllers/map.controller.js
+++ app/controllers/map.controller.js
@@ -10,95 +10,119 @@ exports.convertBook = (req, res, next) => {
 	return convertToGeoJson(req, res, next, "books");
 };
 
-function convertToGeoJson(req, res, next, folder_name) {
+function sanitizeFeature(feature)
+{
+	feature = reduceDecimal(feature);
+
+	// Unclosed Polygons
+	{
+		var first_lat_lon = feature.geometry.coordinates[0][0];
+		var last_lat_lon = feature.geometry.coordinates[0][feature.geometry.coordinates[0].length - 1];
+
+		if ( first_lat_lon[0] != last_lat_lon[0] && first_lat_lon[1] != last_lat_lon[1] )
+		{
+			console.log("Unclosed Polygon: " + feature.properties.PARCEL_NUM);
+			feature.geometry.coordinates[0].push(feature.geometry.coordinates[0][0]);
+		}
+	}
+	
+	if ( feature.geometry.coordinates.length > 1 )
+	{
+		feature.geometry.coordinates.splice(1);
+	}
+	
+	if ( feature.geometry.type == 'MultiPolygon' )
+	{
+		// Convert a multipolygon into a polygon
+		feature.geometry.type = 'Polygon';
+		feature.geometry.coordinates[0] = feature.geometry.coordinates[0][0];
+		console.log("MultiPolygon: "  + feature.properties.PARCEL_NUM);
+	}
+
+	if ( feature.properties.CON_NUMBER != null )
+	{
+		// Con
+	}
+	else if ( feature.properties.DISTRICT != null ) 
+	{
+		// Fire District
+	}
+	else
+	{
+		// Normal Parcel
+
+		if ( feature.properties.PARCEL_NUM == null ) return null; // Skip parcels that have no number
+		if ( feature.properties.PARCEL_NUM.indexOf("INDEX") >= 0 ) return null; // Skip Indexes
+	}
+
+	return feature;
+}
+
+function reduceDecimal(feature)
+{
+	for ( var i = 0; i < feature.geometry.coordinates[0].length; i++ )
+	{
+		var lat_lon = feature.geometry.coordinates[0][i];
+		var reduced = [];
+		reduced.push(lat_lon[0].toFixed(6));
+		reduced.push(lat_lon[1].toFixed(6));
+		feature.geometry.coordinates[0][i] = reduced;
+	}
+	
+	return feature;
+}
+
+function convertToGeoJson(req, res, next, folder_name) 
+{
 	if (!req.files.file || !req.files.file.name) {
 		res.status(400).json({error: true, msg: 'No file provided'})
 		return;
 	  }
 	  
-	  var data = req.files.file;
-		var file_name = data.name.replace(".zip", ".json");
-		var password = req.body.password;
-		
-		if ( password != UPLOAD_PASSWORD ) return res.json({"message": "Invalid Password"});
-
-	  superagent_request
-		  .post('http://ogre.adc4gis.com/convert')
-		  .field('sourceSrs', '')
-		  .field('targetSrs', '')
-		  .attach('upload', data.path)
-		  .end(function (er, res) {
-			  if (er) return console.error(er)
-			  
-			  var sanitized = {};
-			  sanitized.type = res.body.type;
-			  sanitized.features = [];
-  
-			  // Go through each feature and verify that the polygon is properly closed (first coordinate is equal to the last one)
-			  for ( var i = 0; i < res.body.features.length; i++ )
-			  {
-				  var feature = res.body.features[i];
+	var data = req.files.file;
+	var file_name = data.name.replace(".zip", ".json");
+	var password = req.body.password;
 	
-					// Unclosed Polygons
-				  {
-						var first_lat_lon = feature.geometry.coordinates[0][0];
-						var last_lat_lon = feature.geometry.coordinates[0][feature.geometry.coordinates[0].length - 1];
+	if ( password != UPLOAD_PASSWORD ) return res.json({"message": "Invalid Password"});
 
-						if ( first_lat_lon[0] != last_lat_lon[0] && first_lat_lon[1] != last_lat_lon[1] )
-						{
-							console.log("Unclosed Polygon: " + feature.properties.PARCEL_NUM);
-							feature.geometry.coordinates[0].push(feature.geometry.coordinates[0][0]);
-						}
-					}
-					
-					if ( feature.geometry.coordinates.length > 1 )
-				  {
-						feature.geometry.coordinates.splice(1);
-					}
-					
-					if ( feature.geometry.type == 'MultiPolygon' )
-					{
-						// Convert a multipolygon into a polygon
-						feature.geometry.type = 'Polygon';
-						feature.geometry.coordinates[0] = feature.geometry.coordinates[0][0];
-						console.log("MultiPolygon: "  + feature.properties.PARCEL_NUM);
-					}
-  
-				  if ( feature.properties.CON_NUMBER != null )
-				  {
-						// Con
-				  }
-				  else if ( feature.properties.DISTRICT != null ) 
-				  {
-						// Fire District
-				  }
-				  else
-				  {
-						// Normal Parcel
-
-						if ( feature.properties.PARCEL_NUM == null ) continue; // Skip parcels that have no number
-						if ( feature.properties.PARCEL_NUM.indexOf("INDEX") >= 0 ) continue; // Skip Indexes
-				  }
-
-				  sanitized.features.push(feature);
-
-				  // Add to redis cache
-				  if ( feature.properties.PARCEL_NUM != null )
-				  {
-						//console.log("Writing " + feature.properties.PARCEL_NUM + " to Redis");
-						redis_client.set(feature.properties.PARCEL_NUM, JSON.stringify(feature));
-				  }
-			  }
-  
-			  console.log("Finished parsing. Writing file " + file_name);
-			  fs.writeFile(__dirname + "/../../public/" + folder_name + "/" + file_name, JSON.stringify(sanitized), function(err) {
-					if(err) {
-						return console.log(err);
-					}
-				}); 
-			});
+	superagent_request
+		.post('http://ogre.adc4gis.com/convert')
+		.field('sourceSrs', '')
+		.field('targetSrs', '')
+		.attach('upload', data.path)
+		.end(function (er, res) {
+			if (er) return console.error(er)
 			
-			return res.json({"message": "The file was saved as " + file_name});
+			var sanitized = {};
+			sanitized.type = res.body.type;
+			sanitized.features = [];
+
+			// Go through each feature and verify that the polygon is properly closed (first coordinate is equal to the last one)
+			for ( var i = 0; i < res.body.features.length; i++ )
+			{
+				var feature = sanitizeFeature(res.body.features[i]);
+
+				if ( feature == null ) continue;
+				
+				sanitized.features.push(feature);
+
+				// Add to redis cache
+				if ( feature.properties.PARCEL_NUM != null )
+				{
+					//console.log("Writing " + feature.properties.PARCEL_NUM + " to Redis");
+					redis_client.set(feature.properties.PARCEL_NUM, JSON.stringify(feature));
+				}
+			}
+
+			console.log("Finished parsing. Writing file " + file_name);
+			fs.writeFile(__dirname + "/../../public/" + folder_name + "/" + file_name, JSON.stringify(sanitized), function(err) {
+				if(err) {
+					return console.log(err);
+				}
+			}); 
+		});
+		
+		return res.json({"message": "The file was saved as " + file_name});
 }
 
 // Get all GeoJSONs
