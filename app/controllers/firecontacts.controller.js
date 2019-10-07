afb40efff0de795eda200d870ce182c6c03a925b
Jon Toy <jonathon.toy@gmail.com>
2019-10-06 19:54:29 -0700

    Secure Firebase writes

--- app/controllers/firecontacts.controller.js
+++ app/controllers/firecontacts.controller.js
@@ -2,25 +2,28 @@
 
 // Update all Fire Contacts
 const request = require('request');
+const admin = require("firebase-admin");
+var serviceAccount = require("../../apachecountyfirecontact-firebase-adminsdk-rpwly-7322c88f60.json");
+
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount),
+databaseURL: "https://apachecountyfirecontact.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("fire/contacts");
+
 exports.update = (req, res, next) => {
-    console.log(req.body);
-    request.put({
-        url: 'https://apachecountyfirecontact.firebaseio.com/fire/contacts.json', 
-        form: JSON.stringify(req.body),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }, (err,response,body) => {
-        console.log(err);
-        console.log(response);
-        console.log(body);
-        res.send(JSON.parse(body));
-    })
+    ref.set(req.body);
+    res.send({success:"true"});
 };
 
 // Get all Fire Contacts
 exports.list = (req, res, next) => {
-    request.get('https://apachecountyfirecontact.firebaseio.com/fire/contacts.json', (err,response,body) => {
-        res.send(JSON.parse(response.body));
-    })
+    // Attach an asynchronous callback to read the data at our posts reference
+    ref.on("value", function(snapshot) {
+        res.send(snapshot.val());
+    }, function (errorObject) {
+        res.status(500).send("The read failed: " + errorObject.code);
+    });
 };
